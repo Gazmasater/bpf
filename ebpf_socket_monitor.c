a309ebdbe726d314cd48a95a31dfb0c6ef0a71d7
Lev <tolkoplus@gmail.com>
2024-10-25 00:05:28 +0300

    bpf Hello World work

--- ebpf_socket_monitor.c
+++ ebpf_socket_monitor.c
@@ -1,7 +1,6 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
-#define SEC(NAME) __attribute__((section(NAME), used))
 SEC("tracepoint/syscalls/sys_enter_execve")
 int bpf_prog(void *ctx)
 {
