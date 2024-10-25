471f30924231771bd480935a64961f2cfba534ca
gaz358 <tolkoplus@gmail.com>
2024-10-25 12:32:50 +0300

    bpf Hello new

--- ebpf_socket_monitor.c
+++ ebpf_socket_monitor.c
@@ -1,17 +1,13 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
-#include <linux/ptrace.h>
+int counter = 0;
 
-SEC("kprobe/__x64_sys_execve")
-int trace_connect(struct pt_regs *ctx)
+SEC("xdp")
+int hello(void *ctx)
 {
-    // Получаем PID текущего процесса
-    __u32 pid = bpf_get_current_pid_tgid() >> 32;
-
-    // Выводим только PID
-    bpf_trace_printk("PID: %d\n", pid);
-
-    return 0;
+    bpf_printk("Hello World %d", counter);
+    counter++;
+    return XDP_PASS; // Переносим return внутрь функции
 }
 
-char _license[] SEC("license") = "GPL";
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
