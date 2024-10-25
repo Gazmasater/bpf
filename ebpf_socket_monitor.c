4b8ab8e94284e3b8ca00fe8ccdb0c69fc61a39ad
Lev <tolkoplus@gmail.com>
2024-10-25 17:29:13 +0300

    bpf socket

--- ebpf_socket_monitor.c
+++ ebpf_socket_monitor.c
@@ -1,13 +1,35 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
-int counter = 0;
 
-SEC("xdp")
-int hello(void *ctx)
+// Определение структуры bpf_sock_create_args
+struct bpf_sock_create_args
 {
-    bpf_printk("Hello World %d", counter);
-    counter++;
-    return XDP_PASS; // Переносим return внутрь функции
+    __aligned_u64 sock;
+    int family;
+    int type;
+    int protocol;
+    int flags;
+};
+
+SEC("cgroup/sock_create")
+int bpf_sock_create(struct bpf_sock *sk, struct bpf_sock_create_args *ctx)
+{
+    char msg[64];
+    unsigned long long data[3]; // Массив для хранения данных для форматирования
+
+    // Заполняем массив данными
+    data[0] = ctx->family;
+    data[1] = ctx->type;
+    data[2] = ctx->protocol;
+
+    // Используем bpf_snprintf
+    const char *fmt = "Socket created: family=%llu type=%llu protocol=%llu";
+    bpf_snprintf(msg, sizeof(msg), fmt, data, sizeof(data));
+
+    // Запись информации о созданном сокете в dmesg
+    bpf_trace_printk(msg, sizeof(msg));
+
+    return 0;
 }
 
-char LICENSE[] SEC("license") = "Dual BSD/GPL";
+char _license[] SEC("license") = "GPL";
