2b47054a7383fc612e72c90423947cfbd5d449ab
Lev <tolkoplus@gmail.com>
2024-10-26 16:28:21 +0300

    error

--- ebpf_socket_monitor.c
+++ ebpf_socket_monitor.c
@@ -1,23 +1,40 @@
-#include <linux/bpf.h>
+#include <bpf/bpf.h>
 #include <bpf/bpf_helpers.h>
+#include <linux/socket.h>
+#include <linux/ptrace.h>
+#include <linux/sched.h>
+#include <linux/in.h>
+#include <linux/inet.h>
+#include <linux/ip.h>
 
-// Определение структуры bpf_sock_create_args
-struct bpf_sock_create_args
+
+SEC("tracepoint/syscalls/sys_enter_socket")
+int bpf_socket_enter(void *ctx)
 {
-    __aligned_u64 sock;
-    int family;
-    int type;
-    int protocol;
-    int flags;
-};
-
-SEC("cgroup/sock_create")
-int bpf_sock_create(struct bpf_sock_create_args *ctx)
-{
-    bpf_printk("Socket created: family=%d type=%d protocol=%d\n",
-               ctx->family, ctx->type, ctx->protocol);
-
-    return 0;
+    bpf_printk("Socket creation called\n");
+    return 0; 
 }
 
-char _license[] SEC("license") = "GPL";
+SEC("tracepoint/syscalls/sys_exit_socket")
+int bpf_socket_exit(void *ctx)
+{
+    struct bpf_sock *sock; 
+    if (sock)
+    {
+        __be32 local_ip = sock->sk_rcv_saddr; 
+        __be16 local_port = sock->sk_num;   
+
+        // Печатаем адрес и порт
+        bpf_printk("Socket created - Local IP: %d.%d.%d.%d, Local Port: %d\n",
+                   (local_ip >> 0) & 0xFF,
+                   (local_ip >> 8) & 0xFF,
+                   (local_ip >> 16) & 0xFF,
+                   (local_ip >> 24) & 0xFF,
+                   ntohs(local_port));
+    }
+
+    return 0; 
+}
+
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
