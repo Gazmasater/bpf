b3180231fc0b1abd1bad6431d16ebdca5bcd9e60
Lev <tolkoplus@gmail.com>
2024-10-27 23:50:14 +0300

    ip process

--- ebpf_socket_monitor.c
+++ ebpf_socket_monitor.c
@@ -2,7 +2,7 @@
 #include <bpf/bpf_helpers.h>
 #include <linux/socket.h>
 #include <linux/in.h>
-#include <linux/types.h> // Добавлено для определения u32
+#include <linux/types.h> // Для определения u32
 
 SEC("tracepoint/syscalls/sys_enter_bind")
 int bpf_bind_enter(void *ctx)
@@ -16,17 +16,23 @@ int bpf_bind_enter(void *ctx)
     // Получаем PID процесса
     __u32 pid = bpf_get_current_pid_tgid() >> 32;
 
-    // Получаем адрес
+    // Получаем указатель на структуру sockaddr_in
     addr = (struct sockaddr_in *)(ctx + sizeof(int));
 
     // Получаем IP-адрес
     __be32 ip = addr->sin_addr.s_addr;
 
-    // Конвертируем IP в понятный формат
-    unsigned char *ip_bytes = (unsigned char *)&ip;
-    bpf_printk("IP: %u.%u.%u.%u, PID: %u, Process: %s\n",
-               ip_bytes[0], ip_bytes[1], ip_bytes[2], ip_bytes[3],
-               pid, comm);
+    // Проверяем, что адрес сокета является прослушивающим
+    if (addr->sin_port != 0)
+    { // Порт должен быть ненулевым
+        // Конвертируем IP в читаемый формат
+        unsigned char *ip_bytes = (unsigned char *)&ip;
+
+        // Выводим информацию: IP, PID, имя процесса
+        bpf_printk("Listening on IP: %u.%u.%u.%u, PID: %u, Process: %s\n",
+                   ip_bytes[0], ip_bytes[1], ip_bytes[2], ip_bytes[3],
+                   pid, comm);
+    }
 
     return 0;
 }
