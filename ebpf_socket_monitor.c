e3ad013bd54b153c07eef2ee13f124d7a9de0896
Lev <tolkoplus@gmail.com>
2024-10-29 04:08:17 +0300

    di

--- ebpf_socket_monitor.c
+++ ebpf_socket_monitor.c
@@ -1,27 +1,95 @@
-#include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
-#include <linux/socket.h>
-#include <linux/ptrace.h>
-#include <netinet/in.h>
+// go:build ignore
 
-// Функция для обработки входящего сокета
-SEC("tracepoint/syscalls/sys_enter_socket")
-int bpf_socket_enter(struct pt_regs *ctx)
+#include "common.h"
+
+#include "bpf_endian.h"
+#include "bpf_tracing.h"
+
+#define AF_INET 2
+#define TASK_COMM_LEN 16
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+/**
+ * struct sock_common reflects the start of the kernel's struct sock_common.
+ */
+struct sock_common
 {
-    __u32 pid = bpf_get_current_pid_tgid() >> 32; // Получаем PID
-    int domain = (int)ctx->di;                    // Домен сокета
-    int type = (int)ctx->si;                      // Тип сокета
-    int protocol = (int)ctx->dx;                  // Протокол
+    union
+    {
+        struct
+        {
+            __be32 skc_daddr;
+            __be32 skc_rcv_saddr;
+        };
+    };
+    union
+    {
+        __u32 _;
+    };
+    union
+    {
+        struct
+        {
+            __be16 skc_dport;
+            __u16 skc_num;
+        };
+    };
+    short unsigned int skc_family;
+};
 
-    bpf_printk("Socket creation called by PID: %d, Domain: %d, Type: %d, Protocol: %d\n", pid, domain, type, protocol);
-    return 0;
-}
-
-SEC("tracepoint/syscalls/sys_exit_socket")
-int bpf_socket_exit(struct pt_regs *ctx)
+/**
+ * struct sock reflects the start of the kernel's struct sock.
+ */
+struct sock
 {
-    __u32 pid = bpf_get_current_pid_tgid() >> 32; // Получаем PID
+    struct sock_common __sk_common;
+};
 
-    bpf_printk("Socket created by PID: %d\n", pid);
+struct
+{
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 1 << 24);
+} events SEC(".maps");
+
+/**
+ * Struct for data sent to userspace, now includes PID.
+ */
+struct event
+{
+    u8 comm[16];
+    __u16 sport;
+    __be16 dport;
+    __be32 saddr;
+    __be32 daddr;
+    __u32 pid; // Добавленное поле для PID
+};
+struct event *unused __attribute__((unused));
+
+SEC("fentry/tcp_connect")
+int BPF_PROG(tcp_connect, struct sock *sk)
+{
+    if (sk->__sk_common.skc_family != AF_INET)
+    {
+        return 0;
+    }
+
+    struct event *tcp_info;
+    tcp_info = bpf_ringbuf_reserve(&events, sizeof(struct event), 0);
+    if (!tcp_info)
+    {
+        return 0;
+    }
+
+    tcp_info->saddr = sk->__sk_common.skc_rcv_saddr;
+    tcp_info->daddr = sk->__sk_common.skc_daddr;
+    tcp_info->dport = sk->__sk_common.skc_dport;
+    tcp_info->sport = bpf_htons(sk->__sk_common.skc_num);
+    tcp_info->pid = bpf_get_current_pid_tgid() >> 32; // Получение PID
+
+    bpf_get_current_comm(&tcp_info->comm, TASK_COMM_LEN);
+
+    bpf_ringbuf_submit(tcp_info, 0);
+
     return 0;
 }
--- pid.c
+++ pid.c
@@ -11,8 +11,14 @@ SEC("tracepoint/syscalls/sys_enter_socket")
 int bpf_socket_enter(struct pt_regs *ctx)
 {
     __u32 pid = bpf_get_current_pid_tgid() >> 32; // Получаем PID
+    const char *comm = (const char *)bpf_get_current_comm();
+    int domain = PT_REGS_PARM1(ctx);   // Домен (AF_INET, AF_INET6 и т.д.)
+    int type = PT_REGS_PARM2(ctx);     // Тип сокета (SOCK_STREAM, SOCK_DGRAM и т.д.)
+    int protocol = PT_REGS_PARM3(ctx); // Протокол (IPPROTO_TCP, IPPROTO_UDP и т.д.)
 
-    bpf_printk("Socket creation called by PID: %d\n", pid);
+    // Выводим информацию о создании сокета
+    bpf_printk("Socket creation by PID: %d, Command: %s, Domain: %d, Type: %d, Protocol: %d\n",
+               pid, comm, domain, type, protocol);
     return 0; // Возврат 0, чтобы не прерывать выполнение функции
 }
 
@@ -21,6 +27,9 @@ int bpf_socket_exit(struct pt_regs *ctx)
 {
     __u32 pid = bpf_get_current_pid_tgid() >> 32; // Получаем PID
 
+    // Выводим сообщение о завершении создания сокета
     bpf_printk("Socket created by PID: %d\n", pid);
     return 0;
 }
+
+char _license[] SEC("license") = "GPL";
